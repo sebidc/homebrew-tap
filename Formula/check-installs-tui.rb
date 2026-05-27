b84df313cf9cb6bd0b5092148d31c1283675367e
Sebi Dela Cruz <sebi.deezee@icloud.com>
2026-05-27 22:49:15 +0800

    Update check-installs-tui to v1.0.0

--- Formula/check-installs-tui.rb
+++ Formula/check-installs-tui.rb
@@ -1,13 +1,12 @@
 class CheckInstallsTui < Formula
   desc "Terminal UI for browsing installed packages across package managers"
   homepage "https://github.com/sebidc/check-installs-tui"
-  url "https://github.com/sebidc/check-installs-tui/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "c333f5cb8e49065287fc8981379291b1a371385586249d0f88a12a2ae024b866"
+  url "https://github.com/sebidc/check-installs-tui/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "3479fee35d01985ea4cf8705a7dc8977c2455ac1ffa343ce2e78e64a54dec0ec"
   license "MIT"
-
-  depends_on "fzf"
 
   def install
     bin.install "check-installs-tui"
+    bin.install_symlink "check-installs-tui" => "ci"
   end
 end
